73e2b4e384aa52d21a9bff79581f48bbf214366e
Kağan Esen <berk-alp-kaan@hotmail.com>
2024-06-23 17:31:46 +0300

    delete transaction in CreateAccountResponse

--- simplebanking/src/main/java/com/eteration/simplebanking/model/dto/response/CreateAccountResponse.java
+++ simplebanking/src/main/java/com/eteration/simplebanking/model/dto/response/CreateAccountResponse.java
@@ -1,13 +1,11 @@
 package com.eteration.simplebanking.model.dto.response;
 
-import com.eteration.simplebanking.model.entity.Transaction;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -22,6 +20,4 @@ public class CreateAccountResponse {
     private String accountNumber;
 
     private LocalDateTime createDate;
-
-    private List<Transaction> transactions;
 }
